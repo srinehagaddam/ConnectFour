532cf4ca8d5bc6de746503110dd45bf359eedeee
Srineha <srineha.gaddam@gmail.com>
2020-04-01 21:22:24 +0530

    fdsa

---
+++ jquery.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta chatset="utf-8">
+   <title>Main Page</title>
+   <script src="C:\Users\SRINEHA\Documents\CSS\myscript.js"></script>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="C:\Users\SRINEHA\Documents\CSS\Part1_master.css">
+   <link rel="stylesheet" href="https://getbootstrap.com/docs/3.4/javascript/">
+   // <!-- <link rel="stylesheet" href="C:\Users\SRINEHA\Documents\CSS\myscript.js"> -->
+ </head>
+  <body>
+
+  <h1>Welcome to connect Four!</h1>
+  <h2>The object of this game is to connect four of your chips in a row!</h2>
+
+
+
+
+    <script
+  src="http://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
