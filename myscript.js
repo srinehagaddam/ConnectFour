c94fcedb2285caad68860a43dda9691a378d354c
srinehagaddam <63007029+srinehagaddam@users.noreply.github.com>
2020-10-01 13:05:23 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -103,7 +103,7 @@ $('.board button').on('click',function() {
   changeColor(bottomAvail1,col,currentColor);
 
   if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
-    $('h1').text(currentName+" You have won!")
+    $('h1').text(currentName+" You have won! Refresh Your Page")
     $('h3').fadeOut('fast');
     $('h2').fadeOut('fast');
   }
